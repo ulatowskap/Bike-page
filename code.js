9d782f002b770bbb8940c4142819e8130db0fd65
Patrycja <ulatowska.p@gmail.com>
2019-06-27 19:45:34 +0200

    Add new files

---
+++ code.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $(".cross").hide();
+    $(".menu").hide();
+    $(".hamburger").click(function () {
+        $(".menu").slideToggle("slow", function () {
+            $(".hamburger").hide();
+            $(".cross").show();
+        });
+    });
+
+    $(".cross").click(function () {
+        $(".menu").slideToggle("slow", function () {
+            $(".cross").hide();
+            $(".hamburger").show();
+        });
+    });
+
+});
+
+
+ScrollReveal().reveal('.headline');
+
+
+
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000, 'linear');
+});
